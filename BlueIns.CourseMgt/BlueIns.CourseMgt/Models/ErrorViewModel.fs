cc1f059a8c66873dd54460f4e1deb10925aa36cb
jaison ks <ksjaison@gmail.com>
2022-05-26 11:30:36 +0100

    Add project files.

---
+++ BlueIns.CourseMgt/BlueIns.CourseMgt/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace BlueIns.CourseMgt.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
